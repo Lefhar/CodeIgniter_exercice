0263c5f7a473dfef87cf8886d401bc2adfe1812e
harold <s.lefebvre907@laposte.net>
2021-07-11 15:03:22 +0200

    Amélioration de jarditou

--- application/views/detail.php
+++ application/views/detail.php
@@ -1,17 +1,17 @@
 <!-- application/views/detail.php -->
 <div class="container-fluid">
     <div class="row">
-<div class="col-12">   
-<article>
-<div class="card m-4" >
-<?php 
-if(!empty($infoprod)) {
-    foreach ($infoprod as $row) {
-        $stock = "";
-        if (!$row->pro_bloque) {
-            $stock = $row->pro_stock - $row->pro_bloque;
-        }
-        echo ' 
+        <div class="col-12">
+            <article>
+                <div class="card m-4">
+                    <?php
+                    if (!empty($infoprod)) {
+                        foreach ($infoprod as $row) {
+                            $stock = "";
+                            if (!$row->pro_bloque) {
+                                $stock = $row->pro_stock - $row->pro_bloque;
+                            }
+                            echo ' 
     <img  class="img-fluid" width="160" src="' . base_url('assets/images/' . $row->pro_id . '.' . $row->pro_photo . '') . '" alt="' . $row->cat_nom . ' ' . $row->pro_libelle . ' ' . $row->pro_couleur . '" 
     title="' . $row->cat_nom . ' ' . $row->pro_libelle . ' ' . $row->pro_couleur . '">
     <div class="card-body">
@@ -22,16 +22,18 @@ if(!empty($infoprod)) {
       <p class="card-text">Référence : ' . $row->pro_ref . '</p>
       <p class="card-text">catégorie : ' . $row->cat_nom . '</p>
       <p class="card-text">Ajouté le : ' . $row->pro_d_ajout . '</p>';
-      if(!empty($user)){  echo '
+                            if (!empty($user)) {
+                                echo '
       <a href="' . site_url('produits/modifier/' . $row->pro_id) . '" class="btn btn-primary">Modifier</a>   <a href="' . site_url('produits/delete/' . $row->pro_id . '') . '"   class="btn btn-danger">Supprimer le produit</a>
   
-  ';}
+  ';
+                            }
 
-    }
-}
-?>  </div>
-</div>
-                </article>
-</div>
+                        }
+                    }
+                    ?>  </div>
+        </div>
+        </article>
+    </div>
 </div>
 </div>
